994864309e22ceceea94a8e4298c44d45c7af8df
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-23 16:23:54 +0900

    [Add] model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,29 @@
 class Question < ApplicationRecord
+  # validation
+  validates :name, presence: true
+  validates :url,  presence: true, uniqueness: true
+  validates :is_checked, inclusion: {in: [true, false]}
+
+  def scraping_factory
+    itelate_hash_lists(fetch_data)
+  end
+
+  def fetch_data
+    Scraping.new.execute
+  end
+
+  def itelate_hash_lists(hash_lists)
+    hash_lists.each do |hash|
+      Question.find_or_create_by(hash)
+    end
+  end
+
+  def self.select_new_question
+    Question.where(is_checked: false).sample
+  end
+
+  def self.add_check(id)
+    question = Question.find(id)
+    question.update(is_checked: true)
+  end
 end
