a2c2cd5781a243ecc94219c761645f8e8db2dccc
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-01-23 15:20:22 +0900

    [Add] cron

---
+++ lib/cron.rb
@@ -0,0 +1,38 @@
+class Cron
+  attr_reader :question
+
+  def initialize
+    @question = Question.select_new_question
+  end
+
+  def drill_cron
+    if question.blank?
+      create_error_messages
+    else 
+      add_check_today_question
+    end
+    post_slack_bot
+  end
+
+  def post_slack_bot
+    res = Net::HTTP.post_form(
+      URI.parse('https://slack.com/api/chat.postMessage'),
+      {
+        'token': 'xoxb-1554787893026-1680565550705-TJEjG7AntlIIcQsTAp0jNNsR',
+        'channel': '#test',
+        'text': "<!channel> おはようございます！今日も頑張りましょう！！ \n
+                [難易度: abc]#{question.name} \n
+                <#{question.url}>"
+      }
+    )
+  end
+
+  def create_error_messages
+    @question.name = "問題が存在しません"
+    @question.url  = ""
+  end
+
+  def add_check_today_question
+    Question.add_check(question.id)
+  end
+end
